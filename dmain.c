e1e61037e10a7936a3569693b70551438db2cb80
Matan <matan.hrs@gmail.com>
2017-09-17 20:53:21 +0300

    main function now serves as a user interactive version for the game to manually test the environment

--- dmain.c
+++ dmain.c
@@ -16,31 +16,33 @@
 #define PRSTAT	\
 	printf("lives = %d moves = %d score = %d\n",2-state_.deaths, state_.moves, state_.rwscor);
 
-
-int main(int argc,char **argv) {
-/* 1) INITIALIZE DATA STRUCTURES */
-/* 2) PLAY GAME */
-	char string[BUFSIZ];
-
-	FILE* out = freopen("game_output.txt", "w", stdout);
-	if (out==NULL) exit_();
-
-	setbuf(stdout, string);
-	// all output is redirected to the file
+int main(int argc, char **argv) {
+	/* 1) INITIALIZE DATA STRUCTURES */
+	/* 2) PLAY GAME */
+	int i,bad_parse;
+	char buff[1024];
 	if (init_()) {
-    	game_step("OPEN MAILBOX\0");
-    	game_step("READ LEAFLET\0");
-    	game_step("N\0");
-    	game_step("N\0");
-    	game_step("CLIMB THE TREE\0");
-    	game_step("TAKE EGG\0");
-    	PRSTAT
-	init_();
-    	PRSTAT
-    fclose(out);
+		for (i = 0; i < 10000; i++) {
+			/*
+			 *
+			 game_step("OPEN MAILBOX\0");
+			game_step("N\0");
+			game_step("N\0");
+			game_step("CLIMB THE TREE\0");
+			game_step("TAKE EGG\0");
+			PRSTAT
+			init_();
+			printf("inits = %d\n",i);
+			*/
+			rdline_(buff,1);
+			//printf ("%d\n",parse_(buff,1));
+			bad_parse = game_step(buff);
+			printf("bad parse command? %d\naction index: %d\nobject index: %d\ninstrument index %d\n",bad_parse, prsvec_.prsa,prsvec_.prso,prsvec_.prsi);
+		}
+
 	}
-/* 						!IF INIT, PLAY GAME. */
-    exit_();
-/* 						!DONE */
+	/* 						!IF INIT, PLAY GAME. */
+	exit_();
+	/* 						!DONE */
 } /* MAIN__ */
 #endif
